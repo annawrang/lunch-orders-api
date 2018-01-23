11daa2a07b3e0bfae1ab0c7ba83282de7b3a686d
James Griffin <james.griffin@rungway.com>
2018-01-23 16:14:34 +0000

    Tidying the application file

--- src/main/java/com/rungway/LunchOrdersApplication.java
+++ src/main/java/com/rungway/LunchOrdersApplication.java
@@ -5,12 +5,10 @@ import org.springframework.boot.*;
 import org.springframework.boot.autoconfigure.*;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.*;
-import org.springframework.web.bind.annotation.*;
-
 import com.rungway.domain.Restaurant;
 import com.rungway.repositories.RestaurantRepository;
 import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
 public class LunchOrdersApplication {
@@ -20,11 +18,11 @@ public class LunchOrdersApplication {
 
     @EventListener(ApplicationReadyEvent.class)
     public void bootstrapData() {
-        HashMap<String, String> orders = new HashMap<String, String>();
+        Map<String, String> orders = new HashMap<>();
         orders.put("Endre", "Thai Green Curry");
         orders.put("Rohith", "Spidy Chicken Pad Thai");
         orders.put("Ana", "Prawn Pad Thai");
-        orders.put("Harry D", "Chilli Chicken Fried Rice");
+        orders.put("HarryD", "Chilli Chicken Fried Rice");
         
         repo.save(new Restaurant(
             "spicy-basil",
